638fc723418cc052a26346046706a06450a046f1
Lục Lập <boylap7@gmail.com>
2023-12-31 15:28:05 +0700

    getter and array
    add answer for each question, and the answer of it

--- Assets/Scripts/Question.cs
+++ Assets/Scripts/Question.cs
@@ -7,4 +7,21 @@ public class Question : ScriptableObject
 {
     [TextArea(2, 5)]
     [SerializeField] string textQuestion = "Enter a question";
+    [SerializeField] string[] answer = new string[4];
+    [SerializeField] int correctAnswerIndex;
+
+    public string getQuestion()
+    {
+        return textQuestion;
+    }
+
+    public string getAnswer(int index)
+    {
+        return answer[index];
+    }
+
+    public int getCorrectAnswerIndex()
+    {
+        return correctAnswerIndex;
+    }
 }

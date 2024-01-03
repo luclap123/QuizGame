540ee9f975393d7836cf09b4b9b7462d74ecb594
Lục Lập <boylap7@gmail.com>
2024-01-03 15:02:00 +0700

    for loop
    i add loop in my code, it's help me show answer on the screen

--- Assets/Scripts/Quiz.cs
+++ Assets/Scripts/Quiz.cs
@@ -6,11 +6,18 @@ public class Quiz : MonoBehaviour
 {
     [SerializeField] TextMeshProUGUI textQuestions;
     [SerializeField] Question question;
+    [SerializeField] GameObject[] answer;
     // Start is called before the first frame update
     void Start()
     {
         textQuestions.fontSize = 100;
         textQuestions.text = question.getQuestion();
+        for (int i = 0; i < answer.Length; i++)
+        {
+            TextMeshProUGUI textAnswer = answer[i].GetComponentInChildren<TextMeshProUGUI>();
+            textAnswer.text = question.getAnswer(i);
+            textAnswer.color = Color.yellow;
+        }
     }
 
 
